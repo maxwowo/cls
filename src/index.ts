371209fac46ae660478ec804b8de1acb3ad7e871
maxwowo <maxwo@protonmail.com>
2021-03-27 20:28:34 +1100

    Fix lint

--- src/index.ts
+++ src/index.ts
@@ -1,76 +1,83 @@
-type ClassValue = ClassArray | ClassDictionary | string | number | null | boolean | undefined;
+type ClassValue =
+  | ClassArray
+  | ClassDictionary
+  | string
+  | number
+  | null
+  | boolean
+  | undefined;
 
 interface ClassDictionary {
-	[id: string]: any;
+  [id: string]: any;
 }
 
 interface ClassArray extends Array<ClassValue> {}
 
 const dictionaryToClassName = (classObject: ClassDictionary): string => {
-  let className = ''
+  let className = '';
 
   for (const classValue in classObject) {
     if (classObject[classValue]) {
-      className && (className += ' ')
-      className += classValue
+      className && (className += ' ');
+      className += classValue;
     }
   }
 
-  return className
-}
+  return className;
+};
 
 const arrayToClassName = (classArray: ClassArray): string => {
-  let className = ''
+  let className = '';
 
   for (const classValue of classArray) {
     if (classValue) {
-      const flattenedClassValue = toClassName(classValue)
+      const flattenedClassValue = toClassName(classValue);
 
       if (flattenedClassValue) {
-        className && (className += ' ')
-        className += flattenedClassValue
+        className && (className += ' ');
+        className += flattenedClassValue;
       }
     }
   }
 
-  return className
-}
+  return className;
+};
 
 const toClassName = (classValue: ClassValue): string => {
   if (typeof classValue === 'boolean' || !classValue) {
-    return ''
+    return '';
   }
 
   if (typeof classValue === 'string') {
-    return classValue
+    return classValue;
   }
 
   if (typeof classValue === 'number') {
-    return classValue.toString()
+    return classValue.toString();
   }
 
   if (Array.isArray(classValue)) {
-    return arrayToClassName(classValue)
+    return arrayToClassName(classValue);
   }
 
-  return dictionaryToClassName(classValue)
-}
+  return dictionaryToClassName(classValue);
+};
 
 const cls = (...args: ClassValue[]): string => {
-  let classNames = ''
+  let classNames = '';
 
   for (const arg of args) {
     if (arg) {
-      const className = toClassName(arg)
+      const className = toClassName(arg);
 
       if (className) {
-        classNames && (classNames += ' ')
-        classNames += toClassName(arg)
+        classNames && (classNames += ' ');
+        classNames += toClassName(arg);
       }
     }
   }
 
-  return classNames
-}
+  return classNames;
+};
 
-export default cls
+export default cls;
